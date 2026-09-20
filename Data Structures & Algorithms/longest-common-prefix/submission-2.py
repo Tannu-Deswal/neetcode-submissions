ef50410e48283bbbd271c329067533c83923f4b1
Tannu-Deswal <tannudeswal84@gmail.com>
2026-09-20 21:04:14 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return False
+        strs.sort()
+        result = ''
+        for s1, s2 in zip(strs[0], strs[-1]):
+
+            if s1 == s2:
+                result += s1
+            else:
+                break
+        return result
